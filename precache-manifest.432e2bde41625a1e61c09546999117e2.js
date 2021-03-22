2b58b507b36ce922cf549e22dbe087ca150f2ce2
Jagveer Singh <jagveer@jagveer.singh>
2021-03-23 00:45:17 +0530

    Updates

---
+++ precache-manifest.432e2bde41625a1e61c09546999117e2.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "444364688da849882ea58adebaa9fd37",
+    "url": "/ram-stake/index.html"
+  },
+  {
+    "revision": "b45fd96a11bc12d6cdd6",
+    "url": "/ram-stake/static/css/3.214ed77a.chunk.css"
+  },
+  {
+    "revision": "a7b11c3c47890b72fd8d",
+    "url": "/ram-stake/static/js/0.5cf8b5a6.chunk.js"
+  },
+  {
+    "revision": "d4404a8163b2d9fe878c",
+    "url": "/ram-stake/static/js/10.38e4d107.chunk.js"
+  },
+  {
+    "revision": "b45fd96a11bc12d6cdd6",
+    "url": "/ram-stake/static/js/3.3fc67656.chunk.js"
+  },
+  {
+    "revision": "4d227fb27fd06e1f21c21b13e105891f",
+    "url": "/ram-stake/static/js/3.3fc67656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e6a9b5afaf3907fac39",
+    "url": "/ram-stake/static/js/4.eec07344.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "/ram-stake/static/js/4.eec07344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8230ba09ff0e54953a62",
+    "url": "/ram-stake/static/js/5.210302d1.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/ram-stake/static/js/5.210302d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8212fc230558c9d5c966",
+    "url": "/ram-stake/static/js/6.d7888b50.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/ram-stake/static/js/6.d7888b50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e939bce657d80b05e206",
+    "url": "/ram-stake/static/js/7.df338d6b.chunk.js"
+  },
+  {
+    "revision": "ef4bfd726d0271dc85dc",
+    "url": "/ram-stake/static/js/8.d93f3dd8.chunk.js"
+  },
+  {
+    "revision": "72294f56ed26f69bf0f8",
+    "url": "/ram-stake/static/js/9.ee1dc46d.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/ram-stake/static/js/9.ee1dc46d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c39593af67e68f0c8c3",
+    "url": "/ram-stake/static/js/main.17989a33.chunk.js"
+  },
+  {
+    "revision": "ef3c780a473961a200e9",
+    "url": "/ram-stake/static/js/runtime-main.52888b0b.js"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/ram-stake/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/ram-stake/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/ram-stake/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/ram-stake/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/ram-stake/static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
